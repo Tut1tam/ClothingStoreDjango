99e361cc4258a415e8425746d8d5e30b1a1d8e3d
Tut1tam <dolzhich.maxim@yandex.ru>
2023-03-28 18:28:23 +0300

    first commit

---
+++ Store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("about", views.about, name="about"),
+    # path("contact", views.contact, name="contact"),
+    # path("faq", views.faq, name="faq"),
+    # path("products", views.products, name="products"),
+    # path("product/<int:product_id>", views.product, name="product"),
+    # path("cart", views.cart, name="cart"),
+    # path("checkout", views.checkout, name="checkout"),
+    # path("login", views.login_request, name="login"),
+    # path("logout", views.logout_request, name="logout"),
+    # path("register", views.register, name="register"),
+]
---
+++ Store/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "Store/index.html")
+
+
+# def about(request):
+#     return render(request, "Store/about.html")
+
+
+# def contact(request):
+#     return render(request, "Store/contact.html")
+
+
+# def faq(request):
+#     return render(request, "Store/faq.html")
+
+
+# def products(request):
+#     return render(request, "Store/products.html")
+
+
+# def product(request, product_id):
+#     return render(request, "Store/product.html")
+
+
+# def cart(request):
+#     return render(request, "Store/cart.html")
+
+
+# def checkout(request):
+#     return render(request, "Store/checkout.html")
+
+
+# def login_request(request):
+#     return render(request, "Store/login.html")
+
+
+# def logout_request(request):
+#     return render(request, "Store/logout.html")
+
+
+# def register(request):
+#     return render(request, "Store/register.html")
