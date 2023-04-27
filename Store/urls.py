0d5d9311f77de91b8ed901688496da2ede616174
Tut1tam <dolzhich.maxim@yandex.ru>
2023-04-28 00:23:29 +0300

    UPD url.py

--- Store/urls.py
+++ Store/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     # path("about", views.about, name="about"),
     # path("contact", views.contact, name="contact"),
-    # path("faq", views.faq, name="faq"),
     # path("products", views.products, name="products"),
     # path("product/<int:product_id>", views.product, name="product"),
     # path("cart", views.cart, name="cart"),
