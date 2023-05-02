b38bdeb549058c1a54968c86ef3ea8ee7f8e540a
Tut1tam <dolzhich.maxim@yandex.ru>
2023-05-02 21:16:11 +0300

    UPD settings, admin

--- ClothingStore/settings.py
+++ ClothingStore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0mlwi!01imdt-qk$h)t(f^uuu$%3p5cb=&krg%0==jo5p@(lz1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
